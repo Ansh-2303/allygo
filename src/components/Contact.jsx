14badb0c26bc4ea8dee5808ee955523c52ee9e21
Ansh-2303 <anshpatel184@gmail.com>
2026-02-22 13:08:15 +0530

    Restructured project root

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,7 +12,7 @@ function Contact() {
 
         <div className="contact-links">
           <a
-            href="https://instagram.com/YOUR_HANDLE"
+            href="https://www.instagram.com/allygolimited/"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -20,7 +20,7 @@ function Contact() {
           </a>
 
           <a
-            href="https://www.instagram.com/allygolimited/"
+            href="https://www.linkedin.com/company/allygo/?lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3BMXLOUl%2BqT8%2Ba%2BDew%2F4rgBw%3D%3D"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -28,7 +28,7 @@ function Contact() {
           </a>
 
           <a
-            href="https://www.linkedin.com/company/allygo/?lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3BMXLOUl%2BqT8%2Ba%2BDew%2F4rgBw%3D%3D"
+            href="https://whatsapp.com/channel/0029Vb7Mj9Y7tkjAtHYuD20R"
             target="_blank"
             rel="noopener noreferrer"
           >
